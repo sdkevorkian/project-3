26809b1ae9eee1073eccaacf8f7507c039f6cb9c
sdkevorkian <sdkevorkian@gmail.com>
2017-05-22 15:48:29 -0700

    testing with sending data to compare route

--- controllers/pets.js
+++ controllers/pets.js
@@ -4,33 +4,33 @@ var router = express.Router();
 var key = process.env.API_KEY;
 
 router.route('/random')
-  .get(function(req, res) {
-      request(url + 'pet.getRandom' + key + '&format=json', function(err, res, body) {
-          if (err) return ('something went wrong', err);
-          // request returns random petId.  need a 2nd request to get actual info on pet
-      });
-  });
+    .get(function(req, res) {
+        request(url + 'pet.getRandom' + key + '&format=json', function(err, res, body) {
+            if (err) return ('something went wrong', err);
+            // request returns random petId.  need a 2nd request to get actual info on pet
+        });
+    });
 
-
+// show all pets
 router.route('/')
-  .post(function(req, res) {
-      request({
-        uri: 'http://api.petfinder.com/pet.find',
-        method: 'GET',
-        qs: {
-          key: key,
-          animal: req.body.animal,
-          breed: req.body.breed,
-          size: req.body.size,
-          sex: req.body.sex,
-          location: req.body.location,
-          format: 'json'
-        }
-      }, function(error, response, body) {
-          if (error) console.log('somethign went wrong', error);
-          res.send(JSON.parse(body));
-      });
-  });
+    .post(function(req, res) {
+        request({
+            uri: 'http://api.petfinder.com/pet.find',
+            method: 'GET',
+            qs: {
+                key: key,
+                animal: req.body.animal,
+                breed: req.body.breed,
+                size: req.body.size,
+                sex: req.body.sex,
+                location: req.body.location,
+                format: 'json'
+            }
+        }, function(error, response, body) {
+            if (error) console.log('somethign went wrong', error);
+            res.send(JSON.parse(body));
+        });
+    });
 
 
 module.exports = router;
