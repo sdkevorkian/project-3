db54a1a8d96f68503fd6063371816a304b9723d7
jigglor <andrewtsuji@gmail.com>
2017-05-23 11:21:57 -0700

    got search for working/breeds dropdown

--- public/app/pet-controllers.js
+++ public/app/pet-controllers.js
@@ -1,13 +1,13 @@
 angular.module('PetCtrls', [])
     .controller('SearchCtrl', ['$scope', '$http', function($scope, $http) {
         $scope.getBreeds = function() {
-            $http.post('/api/pets/breeds', { animal: $scope.pet.animal }).then(function(data) {
-                $scope.breeds = data;
+            $http.post('/api/pets/breeds', { animal: $scope.pet.animal }).then(function(results) {
+                $scope.breeds = results.data;
+                console.log($scope.breeds);
+                return $scope.breeds;
             }).catch(function(err) {
                 console.log(err);
             });
-            console.log($scope.breeds);
-            return $scope.breeds;
         }
     }])
     .controller('PetShowCtrl', ['$scope', function($scope) {
