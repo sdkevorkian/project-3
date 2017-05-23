59c527acb22705b8e9bf1c640b95412c66cad1d3
sdkevorkian <sdkevorkian@gmail.com>
2017-05-23 12:29:06 -0700

    made links to petfinder

--- public/app/pet-controllers.js
+++ public/app/pet-controllers.js
@@ -19,12 +19,14 @@ angular.module('PetCtrls', [])
         $http.get('/api/pets/' + $stateParams.id).then(function(result){
             $scope.pet =result.data.petfinder.pet;
             localStorage.petUrl=result.data.petfinder.pet.media.photos.photo[2].$t;
-            localStorage.petId = $stateParams.id;
+            $scope.petId = $stateParams.id;
+            localStorage.petId = $scope.petId;
         });
     }])
     .controller('CompareCtrl', ['$scope', '$http', 'Auth', function($scope, $http, Auth) {
         var user = Auth.currentUser();
         $http.post('/api/compare', { userUrl: user.profileImg, petUrl: localStorage.petUrl }).then(function(result) {
+            $scope.petId = localStorage.petId;
             $scope.matchPercent = result.data.matchPercent;
         }).catch(function(err) {
             console.log(err);
