ab2310dc980055bd64f7a409d69d9280c2d9f905
sdkevorkian <sdkevorkian@gmail.com>
2017-05-25 12:58:50 -0700

    added facebook button

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,5 @@
+//  Twitter button code
+
 window.twttr = (function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0],
         t = window.twttr || {};
@@ -14,3 +16,13 @@ window.twttr = (function(d, s, id) {
 
     return t;
 }(document, "script", "twitter-wjs"));
+
+// facebook button code
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=155807374951752";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
